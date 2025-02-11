b591dec6e78118514ff0d433d1776fc7d355ecc1
David Isinta <david.isinta@gmail.com>
2025-02-10 21:40:24 -0500

    basic sdl app running

---
+++ 01_SDL_OpenGL_Setup/main.cpp
@@ -0,0 +1,63 @@
+// On linux compile with:
+// g++ -std=c++17 main.cpp -o prog -lSDL2
+
+// C++ Standard Libraries
+#include <iostream>
+
+// Third-party library
+#include <SDL2/SDL.h>
+
+using namespace std;
+
+
+void endGame(SDL_Window* window){
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    cout << "ending the game!! \n";
+}
+
+int main(int argc, char* argv[]){
+
+    // Create a window data type
+    // This pointer will point to the 
+    // window that is allocated from SDL_CreateWindow
+    SDL_Window* window=nullptr;
+
+    // Initialize the video subsystem.
+    // If it returns less than 1, then an
+    // error code will be received.
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        std::cout << "SDL could not be initialized: " <<
+                  SDL_GetError();
+    }else{
+        std::cout << "SDL video system is ready to go\n";
+    }
+
+    // Request a window to be created for our platform
+    // The parameters are for the title, x and y position,
+    // and the width and height of the window.
+    window = SDL_CreateWindow("C++ SDL2 Window",
+            0,
+            2500,
+            640,
+            480,
+            SDL_WINDOW_SHOWN);
+
+    // Infinite loop for our application
+    bool gameIsRunning = true;
+    while(gameIsRunning){
+        SDL_Event event;
+        // Start our event loop
+        while(SDL_PollEvent(&event)){
+            // Handle each specific event
+            if(event.type == SDL_QUIT){
+                gameIsRunning= false;
+                endGame(window);
+            }
+
+            
+        }
+    }
+
+    return 0;
+}
